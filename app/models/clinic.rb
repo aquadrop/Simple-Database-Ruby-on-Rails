b263a7b039c028d7796535a6758bf0ed38268075
Jianwei Xu <forestserenity@gmail.com>
2013-06-12 20:50:55 -0300

    Update clinic.rb

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,5 @@
 class Clinic < ActiveRecord::Base
   attr_accessible :id, :name
-  validate :name, :presence => true
+  validates :name, :presence => true
   has_many :doctors, :dependent => :destroy
 end
