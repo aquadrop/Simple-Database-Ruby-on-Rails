b4768c2bb6ec2548bf37291046a0e50783a7a6e8
Jianwei Xu <forestserenity@gmail.com>
2013-06-12 20:51:13 -0300

    Update doctor.rb

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ActiveRecord::Base
   belongs_to :clinic
   attr_accessible :id, :name
-  validate :name, :presence => true,
+  validates :name, :presence => true,
                   :uniqueness => true
   default_scope :order => 'doctors.created_at DESC'
 end
